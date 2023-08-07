8be7876a493f3c3fdd24c1806e77dc53347395d4
MarkoPC <minovskimarco@gmail.com>
2023-08-07 05:41:47 +0200

    Chat functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,130 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/analytics';
+
+import { doc, deleteDoc } from "firebase/compat/firestore";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCBri9L4EQVzD8XbgGfTpbJy6Flyp73O9o",
+  authDomain: "random-chat-app-7f3ca.firebaseapp.com",
+  projectId: "random-chat-app-7f3ca",
+  storageBucket: "random-chat-app-7f3ca.appspot.com",
+  messagingSenderId: "147505168002",
+  appId: "1:147505168002:web:4ccbc9c78a83c37672be9b",
+  measurementId: "G-HPYL8FRZMJ"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="bg-cyan-700">
-          testing
-        </div>
-      </header>
-    </div>
+      <div className="App">
+        <header>
+          <h1>ChatTwist</h1>
+          <SignOut />
+        </header>
+
+        <section>
+          {user ? <ChatRoom /> : <SignIn />}
+        </section>
+
+      </div>
   );
 }
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+      <>
+        <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const scrollToBottomDiv = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(50);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    scrollToBottomDiv.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={scrollToBottomDiv}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) =>
+          setFormValue(e.target.value)} placeholder="Message..." />
+
+      <button type="submit" disabled={!formValue}>Send</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
 
 export default App;
