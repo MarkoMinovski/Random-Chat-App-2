a2c03da48cb38588901e259ffb895a4c24c3ff03
minov <minovskimarco@gmail.com>
2023-10-08 22:56:24 +0200

    Fixed incompatibility issues between namespaced Firebase 8 code and Modular Firebase 9 code
    - Added subcollection functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 
+
 import firebase from 'firebase/compat/app';
+
+import { initializeApp } from 'firebase/app';
+
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/analytics';
 
-import { addDoc, collection, doc, updateDoc } from 'firebase/firestore';
 
-import { deleteDoc } from "firebase/compat/firestore";
+import { getAuth, getAdditionalUserInfo, signInWithPopup } from 'firebase/auth';
+
+import {collection, doc, getDocs, updateDoc, query,
+     where, getFirestore, getDoc, addDoc, setDoc } from 'firebase/firestore';
+
+
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 
-firebase.initializeApp({
+const config = ({
   apiKey: "AIzaSyCBri9L4EQVzD8XbgGfTpbJy6Flyp73O9o",
   authDomain: "random-chat-app-7f3ca.firebaseapp.com",
   projectId: "random-chat-app-7f3ca",
@@ -24,15 +32,35 @@ firebase.initializeApp({
   measurementId: "G-HPYL8FRZMJ"
 })
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+initializeApp(config);
+
+const auth = getAuth();
+const firestore = getFirestore();
+
+let matched = false;
+
+let globalOtherUser;
 
 
 function App() {
 
+
   const [user] = useAuthState(auth);
 
+    let content;
+
+    if (user && !matched) {
+        content = <Menu />;
+    } else if (!user) {
+        content = <SignIn />;
+    } else if (user && matched) {
+        content = <DisplayLatestRoom />
+    }
+
+
   return (
+
+
       <div className="App">
         
         <header>
@@ -41,7 +69,7 @@ function App() {
         </header>
 
         <section>
-          {user ? <ChatRoom /> : <SignIn />}
+            {content}
         </section>
 
 
@@ -49,16 +77,21 @@ function App() {
   );
 }
 
+
+
 function SignIn() {
-  const usersRef = firestore.collection('users');
+  const usersRef = collection(firestore, "users");
   const signInWithGoogle = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
+    signInWithPopup(auth, provider)
         .then( async (result) => {
-            await usersRef.add({
+            const user = getAdditionalUserInfo(result);
+            if (user.isNewUser) {
+                const data = ({
                     userid: auth.currentUser.uid
-                }
-            )
+                })
+                await addDoc(usersRef, data);
+            }
         })
   }
 
@@ -83,52 +116,6 @@ function SignOut() {
   )
 }
 
-
-function ChatRoom() {
-  const scrollToBottomDiv = useRef();
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(50);
-
-  const [messages] = useCollectionData(query, { idField: 'id' });
-
-  const [formValue, setFormValue] = useState('');
-
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    const { uid, photoURL } = auth.currentUser;
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL
-    })
-
-    setFormValue('');
-    scrollToBottomDiv.current.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  return (<>
-    <main>
-
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
-      <span ref={scrollToBottomDiv}></span>
-
-    </main>
-
-    <form onSubmit={sendMessage}>
-
-      <input value={formValue} onChange={(e) =>
-          setFormValue(e.target.value)} placeholder="Message..." />
-
-      <button type="submit" disabled={!formValue}>Send</button>
-
-    </form>
-  </>)
-}
 
 
 function ChatMessage(props) {
@@ -158,25 +145,216 @@ function ChatMessage(props) {
 }
 
 async function counterIncrementer() {
-    let counterRef =
-        doc(firestore, "counter", "Room Counter");
+    const counterRef =
+        doc(firestore, 'counter/Room Counter');
 
-    let counter = await counterRef.get()
-        .then(doc => {
-            return doc.data().room_count;
-        }) .catch(err => {
-            console.log('Error 5', err);
-        });
+    const result = await getDoc(counterRef);
 
-    counter = counter + 1;
+    let { room_count } = result.data();
+
+    let counter = room_count;
+
+
+    counter += 1;
 
     const updatedData = {
         room_count: counter
     }
 
-    updateDoc(counterRef, updatedData);
+    await updateDoc(counterRef, updatedData);
 }
 
 
+
+async function MatchUsers(user) {
+
+    const query2 = query(collection(firestore, 'users'),
+        where("userid", "!=", auth.currentUser.uid));
+
+    const queryResult = await getDocs(query2);
+
+    const docArray = queryResult.docs;
+
+    const snapshotCount = queryResult.size;
+    const randomNumber = Math.floor(Math.random() * snapshotCount);
+
+    let counter = 0;
+    let otherUser, temp;
+
+
+    docArray.forEach(doc => {
+        if (counter === randomNumber) {
+            temp = doc.data();
+            console.log(temp);
+        } else
+            counter++;
+    })
+
+    const { userid } = temp;
+
+    otherUser = userid;
+
+    await counterIncrementer();
+
+    globalOtherUser = otherUser;
+
+    await CreateNewChatRoom();
+
+    matched = true;
+
+}
+
+async function CreateNewChatRoom() {
+    let counterRef =
+        doc(firestore, "counter", "Room Counter");
+
+    const docResult = await getDoc(counterRef);
+
+    const { room_count } = docResult.data();
+
+    const counter = room_count;
+
+    const userData = {
+        user1: auth.currentUser.uid,
+        user2: globalOtherUser,
+        createdOn: firebase.firestore.FieldValue.serverTimestamp()
+    }
+
+    await setDoc(doc(firestore, "rooms", "room"+counter),
+        userData);
+
+    //const messagesRef
+    //    = firestore.doc('rooms/room'+counter).collection('messages');
+
+    const messagesRef
+        = collection(firestore, "rooms", "room"+counter, "messages");
+
+
+    //await setDoc(testing, testing3, {merge: true})
+
+    const {uid, photoURL} = auth.currentUser;
+
+    /*await messagesRef.add( {
+        text: '',
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL
+    })*/
+
+    const messageData = {
+        text: '',
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL
+    }
+
+    await addDoc(messagesRef, messageData);
+
+}
+
+function Menu() {
+
+    const [user] = useAuthState(auth);
+
+    return ( <>
+
+            <div className="dud">
+                &nbsp;
+            </div>
+
+            <button className="sign-in" onClick={() => MatchUsers(user)}>
+            Match with someone!</button>
+
+            <div className="dud">
+                &nbsp;
+            </div>
+
+        </>
+    )
+}
+
+async function DisplayLatestRoom() {
+
+    const roomsCol = collection(firestore, 'rooms');
+
+    const firstQuery =
+        query(roomsCol, where("user1", "==", auth.currentUser.uid));
+
+    const secondQuery
+        = query(roomsCol, where("user2", "==", auth.currentUser.uid));
+    //No other way to do this that I can find. :(
+
+    const activeSnapshot = await getDocs(firstQuery);
+    const secondarySnapshot = await getDocs(secondQuery);
+    const merged = [];
+
+    activeSnapshot.forEach((doc) => {
+        merged.push(doc.data());
+    });
+    secondarySnapshot.forEach((doc) => {
+       merged.push(doc.data());
+    });
+
+    const [targetRoomArray] = merged.docs();
+    let targetRoom = {
+        id: ''
+    };
+
+    let localCounter = 0;
+
+    targetRoomArray.forEach(doc => {
+        if (localCounter === 0) {
+            targetRoom.id = doc.id;
+            localCounter++;
+        }
+    })
+
+    const scrollToBottomDiv = useRef();
+    const messagesRef
+        = firestore.doc(targetRoom.id).collection('messages');
+
+    const messagesQuery = messagesRef.orderBy('createdAt').limit(50);
+
+    const [messages] = useCollectionData(messagesQuery, { idField: 'id' });
+
+    const [formValue, setFormValue] = useState('');
+
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        const { uid, photoURL } = auth.currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        })
+
+        setFormValue('');
+        scrollToBottomDiv.current.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    return (<>
+        <main>
+
+            {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+            <span ref={scrollToBottomDiv}></span>
+
+        </main>
+
+        <form onSubmit={sendMessage}>
+
+            <input value={formValue} onChange={(e) =>
+                setFormValue(e.target.value)} placeholder="Message..." />
+
+            <button type="submit" disabled={!formValue}>Send</button>
+
+        </form>
+    </>)
+
+}
 
 export default App;
